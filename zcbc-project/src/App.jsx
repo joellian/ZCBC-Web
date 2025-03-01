4ad34d82805f105663ce169d56f2bf8e9613e9ad
Joel <Lianb1@nku.edu>
2025-03-01 18:25:20 -0500

    Implemented ReactJS and Vite to the project along with updated README.md

---
+++ zcbc-project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import VisitUs from './components/VisitUs'
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <VisitUs />
+
+      
+    </div>
+  )
+}
+
+export default App
---
+++ zcbc-project/src/components/VisitUs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const VisitUs = () => {
+    return (
+        <div ClassName="VisitUs">
+            <h1> LOCATED IN NORTHERN KENTUCKY </h1>
+            <h3> We are located in Main Street Florence </h3>
+            <h1> OUR ADDRESS </h1>
+            <h3> Zion Chin Baptist Church</h3>
+            <h3> 213 Main Street </h3>
+            <h3> Florence, KY 41042 </h3>
+            <h1> SUNDAY GATHERING TIMES </h1>
+            <h3> Sunday School </h3>
+            <h4> 12:00 PM to 2:00 PM </h4>
+            <h3> The Chapel Service </h3>
+            <h4> 2:00 PM to 4:00 PM</h4>
+
+        </div>
+
+    )
+}
+
+export default VisitUs;
+
